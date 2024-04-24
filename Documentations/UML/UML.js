510fde931bf8aadf81ac7a870323d50aacad49d6
Yannick Perret <contact@yannickperret.com>
2024-04-24 13:50:57 +0200

    added UML v1

---
+++ Documentations/UML/UML.js
@@ -0,0 +1,144 @@
+classDiagram
+class BaseModel {
+<<Interface>>
++save()
++delete()
++update()
++find()
+}
+class AuthFinder {
+<<Interface>>
++findAuth(uid: string): User
++verifyCredentials(uid: string, password: string): boolean
+}
+
+class User {
++int Id_Users
++String Email
++String Password
++String FullName
++DateTime LastLoginAt
+}
+class Playlist {
++int Id_Playlists
++String Name
++String FilePath
++Video[] videos
++load() Promise
++generateFile(type: string) Promise
++getCurrentVideo() Video
++getNextVideo(showTransition: boolean) Video
++getPreviousVideo() Video
++moveToNextVideo()
++removeCurrentVideo() Promise
+}
+class Video {
++int Id_Videos
++String Title
++String Path
++int Duration
++bool ShowInNext
++getInformation(path: string) Promise
++getDuration(path: string) Promise
++elapsedTime() Promise
++getDurationInSeconds() Promise
++getDurationInMilliseconds() Promise
+}
+class Timeline {
++int Id_Timelines
++String Name
+}
+class Stream {
++int Id_Streams
++String Name
++int Pid
++String Status
++DateTime StartTimestamp
++DateTime EndTimestamp
++String Parameters
++Provider[] providers
+
++getUptime() Promise
++updateRunnerText() Promise
++updateNextGameText() Promise
++nextVideo() Promise
++run() Promise
++startStream(Playlist) Promise
++restartStream() Promise
++stopStream() Promise
+}
+class Provider {
++int Id_Providers
++String Name
++String Type
++String ClientId
++String ClientSecret
++String RefreshToken
++String BroadcasterId
++String AuthBearer
++String StreamKey
++bool OnPrimary
++createProvider(Provider) Promise
+}
+
+class Twitch {
+-static instance: Twitch
+-tmi.Client client
+-changeCategory(String) Promise
+-changeTitle(String, Video) Promise
+-changeTags(String[]) Promise
+-sendChatMessage()
+-refreshTokenWhenExpired() Promise
+-BASE_URL_API: String
+}
+
+class Queue {
+-QueueItem[] queue
+-bool isEncoding
+-static instance: Queue
++int id
++DateTime createdAt
++DateTime updatedAt
++add(Video, string, string) Promise
+-runEncoding() Promise
+-processQueue() Promise
++static getInstance() Queue
+}
+
+class QueueItem {
++Video video
++string startTimeCode
++string endTimeCode
++resolve(value: string)
++reject(reason: any)
+}
+class VideoEncoder {
++int id
++DateTime createdAt
++DateTime updatedAt
++static encode(Video, string, string, string) Promise
+}
+
+
+AuthFinder <|-- User
+BaseModel <|-- User
+BaseModel <|-- Playlist
+BaseModel <|-- Video
+BaseModel <|-- Timeline
+BaseModel <|-- Stream
+BaseModel <|-- Provider
+Provider <|-- Twitch
+BaseModel <|-- Queue
+BaseModel <|-- VideoEncoder
+Queue "1" -- "*" QueueItem
+QueueItem "1" -- "1" Video
+VideoEncoder -- Video
+
+User "1" *-- "*" Playlist
+User "1" *-- "*" Stream
+User "1" -- "*" Timeline
+Playlist "1" *-- "*" Video
+Timeline "1" *-- "*" Video
+Timeline "1" *-- "*" Playlist
+Stream "1" o-- "*" Provider
+Stream "1" -- "1" Timeline
