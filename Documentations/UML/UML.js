9331168fc2d7968740d65568bb79382252dbc7f5
Yannick Perret <contact@yannickperret.com>
2024-06-05 02:18:39 +0200

    docs : update  uml for v4 guest token

--- Documentations/UML/UML.js
+++ Documentations/UML/UML.js
@@ -342,3 +342,195 @@ TimelineElements "0..1" -- "0..1" Playlist : playlist
 Stream "1" o-- "*" Provider
 Stream "1" -- "1" Timeline
 Guest "*" -- "0..*" Video : videos
+
+
+/**** UML V4 ****/
+classDiagram
+class AuthFinder {
+<<Interface>>
+    +findAuth(uid: string): User
+    +verifyCredentials(uid: string, password: string): boolean
+}
+
+class User {
+    +int id
+    +String email
+    +String password
+    +String fullName
+    +DateTime lastLoginAt
+    +DateTime createdAt
+    +DateTime updatedAt
+    +streams: Stream[]
+}
+
+class Guest {
+    +int id
+    +String email
+    +String displayName
+    +String ipAddress
+    +String discordUsername
+    +String steamUsername
+    +String twitchUsername
+    +String twitterUsername
+    +String youtubeUsername
+    +String telegramUsername
+    +int canDiffuse
+    +String notes
+    +DateTime createdAt
+    +DateTime updatedAt
+}
+
+class GuestToken {
+    +int id
+    +int guestId
+    +int videoId
+    +String token
+    +String status
+    +DateTime expiresAt
+    +DateTime createdAt
+    +DateTime updatedAt
+}
+
+class Playlist {
+    +int id
+    +String title
+    +String description
+    +boolean isPublished
+    +int userId
+    +DateTime createdAt
+    +DateTime updatedAt
+    +videos: Video[]
+}
+
+class PlaylistVideo {
+    +int id
+    +int playlistId
+    +int videoId
+    +int order
+    +DateTime createdAt
+    +DateTime updatedAt
+}
+
+class Provider {
+    +int id
+    +String name
+    +String streamKey
+    +String type
+    +String authBearer
+    +String clientId
+    +String clientSecret
+    +String refreshToken
+    +String accessToken
+    +String broadcasterId
+    +int userId
+    +DateTime createdAt
+    +DateTime updatedAt
+}
+
+class Queue {
+    +int id
+    +String title
+    +boolean active
+    +int maxSlots
+    +int maxConcurrent
+    +DateTime createdAt
+    +DateTime updatedAt
+    +items: QueueItem[]
+}
+
+class QueueItem {
+    +int id
+    +int queueId
+    +int videoId
+    +String startTimeCode
+    +String endTimeCode
+    +int attempts
+    +String status
+    +DateTime createdAt
+    +DateTime updatedAt
+}
+
+class Stream {
+    +int id
+    +String name
+    +int pid
+    +String status
+    +int restartTimes
+    +int currentIndex
+    +String type
+    +int userId
+    +int providerId
+    +int timelineId
+    +String overlay
+    +String guestFile
+    +String cryptoFile
+    +String logo
+    +DateTime startTime
+    +DateTime endTime
+    +DateTime createdAt
+    +DateTime updatedAt
+    +providers: Provider[]
+}
+
+class Timeline {
+    +int id
+    +String title
+    +String description
+    +String filePath
+    +boolean isPublished
+    +boolean showInLive
+    +int userId
+    +int streamId
+    +DateTime createdAt
+    +DateTime updatedAt
+    +items: TimelineItem[]
+}
+
+class TimelineItem {
+    +int id
+    +int timelineId
+    +int order
+    +String type
+    +int itemId
+    +DateTime createdAt
+    +DateTime updatedAt
+}
+
+class Video {
+    +int id
+    +String title
+    +String description
+    +String path
+    +int duration
+    +String status
+    +int showInLive
+    +int userId
+    +int guestId
+    +DateTime createdAt
+    +DateTime updatedAt
+}
+
+class VideoEncoder {
+    +int id
+    +DateTime createdAt
+    +DateTime updatedAt
+    +static encode(Video, string, string, string) Promise
+}
+
+AuthFinder <|-- User : uses
+User "1" --> "*" Playlist : owns
+User "1" --> "*" Stream : owns
+User "1" --> "*" Timeline : owns
+User "1" --> "*" Provider : creates
+Playlist "1" --> "*" Video : includes
+Playlist "1" --> "*" PlaylistVideo : includes
+Timeline "1" --> "*" TimelineItem : contains
+TimelineItem "0..1" --> "0..1" Video : refers to
+TimelineItem "0..1" --> "0..1" Playlist : refers to
+Stream "1" --> "*" Provider : uses
+Stream "1" --> "1" Timeline : references
+Guest "1" --> "0..*" Video : owns
+Guest "1" --> "0..*" GuestToken : possesses
+Queue "1" --> "*" QueueItem : contains
+QueueItem "1" --> "1" Video : processes
+VideoEncoder --> Video : encodes
